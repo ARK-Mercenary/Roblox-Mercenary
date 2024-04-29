abe9104783288672e5776bc6c457bcac811f81fe
ARK-Mercenary <164679412+ARK-Mercenary@users.noreply.github.com>
2024-04-29 03:35:55 -0500

    Add files via upload

---
+++ Utilities/CryptUtilities.lua
@@ -0,0 +1,81 @@
+--[[ 
+    This utility library utilizes the built in Executor functions in the 'crypt' table.
+    I'm not sure what Executors have these functions, but to my knowledge, 
+    Krampus Windows, CodeX Android/iOS, and Hydrogen MacOS both support these functions.
+    I have not tested any other Executor. I think Wave Windows, and Acerus X Android/iOS work but I don't know.
+
+    I've created this utility to simplify the usage of these functions for myself and to enhance the readability of my code for others.
+
+    This code isn't commented at all, mainly because of how simple and self explanatory it is.
+    Realistically you don't need to use this, but I feel like it makes your code cleaner.
+
+    Unfortunately, the 'crypt' table doesn't support every single hash algorithm, but I have included
+    all the ones it has here.
+
+    List of supported algorithms if you don't wanna read the code:
+        Encryption: GCM AES Encryption & Decryption
+        Hashing: md5, sha1, sha3-224, sha3-256, and sha3-512
+        Other: 
+]]
+
+local KleenexUtils = loadstring(game:HttpGet(""))()
+
+local CryptUtilities = {} do
+    CryptUtilities.Encrypt = crypt.encrypt
+    CryptUtilities.Decrypt = crypt.decrypt
+    CryptUtilities.Hash = crypt.hash
+
+    function CryptUtilities:Encrypt(data, key, iv)
+        task.spawn(function()
+            return
+        end)
+    end
+
+    function CryptUtilities:Decrypt(data, key, iv)
+        task.spawn(function()
+            return
+        end)
+    end
+
+    function CryptUtilities:MD5(data)
+        task.spawn(function()
+            KleenexUtils:CheckFunctionExists(self.Hash)
+
+            return self.Hash(data, "md5")
+        end)
+    end
+
+    function CryptUtilities:SHA1(data)
+        task.spawn(function()
+            KleenexUtils:CheckFunctionExists(self.Hash)
+
+            return self.Hash(data, "sha1")
+        end)
+    end
+
+    function CryptUtilities:SHA224(data)
+        task.spawn(function()
+            KleenexUtils:CheckFunctionExists(self.Hash)
+
+            return self.Hash(data, "sha3-224")
+        end)
+    end
+
+    function CryptUtilities:SHA256(data)
+        task.spawn(function()
+            KleenexUtils:CheckFunctionExists(self.Hash)
+
+            return self.Hash(data, "sha3-256")
+        end)
+    end
+
+    function CryptUtilities:SHA512(data)
+        task.spawn(function()
+            KleenexUtils:CheckFunctionExists(self.Hash)
+
+            return self.Hash(data, "sha3-512")
+        end)
+    end
+end
+
+return CryptUtilities
---
+++ Utilities/KleenexUtilities.lua
@@ -0,0 +1,99 @@
+local HTTPRequest = syn and syn.request or http and http.request or http_request or fluxus and fluxus.request or request
+local Players = game:GetService("Players")
+local HttpService = game:GetService("HttpService")
+local LocalPlayer = Players.LocalPlayer
+
+local KleenexUtilities = {} do
+    KleenexUtilities.Folder = "Kleenex/Utilities/"
+
+    function KleenexUtilities:SendWebhookMessage(url, message, embed)
+        task.spawn(function()
+            if not #url > 0 then return end
+            if not url or not message or not embed then
+                return
+            end
+
+            self:CheckFunctionExists(HTTPRequest)
+            
+            HTTPRequest({
+                Url = url,
+                Method = "POST",
+                Headers = {
+                    ["Content-Type"] = "application/json"
+                },
+                Body = HttpService:JSONEncode({
+                    content = message, embeds = { 
+                        embed 
+                    } 
+                })
+            })
+        end)
+    end
+
+    function KleenexUtilities:CheckFunctionExists(functionName)
+        task.spawn(function()
+            local IsValid = functionName and getgenv()[functionName]
+            
+            if not IsValid then
+                LocalPlayer:Kick(string.format("Executor Not Supported. Missing: %s", functionName))
+                return
+            end
+        end)
+    end
+
+    function KleenexUtilities:CheckFunctionExistsTable(functionNames)
+        task.spawn(function()
+            for _, functionName in ipairs(functionNames) do
+                local IsValid = functionName and getgenv()[functionName]
+                
+                if not IsValid then
+                    LocalPlayer:Kick(string.format("Executor Not Supported. Missing: %s", functionName))
+                    return
+                end
+            end
+        end)
+    end
+
+    function KleenexUtilities:GetExecutions()
+        task.spawn(function()
+            self:CheckFunctionExistsTable({ "isfolder", "isfile", "makefolder", "makefile" })
+
+            if not isfolder(self.Folder) then 
+                makefolder(self.Folder) end
+            if not isfile(self.Folder .. "Executions.txt") then  
+                writefile(self.Folder .. "Executions.txt", "1") end
+            
+            writefile(self.Folder .. "Executions.txt", tostring(readfile(self.Folder .. "Executions.txt") or 0) + 1)
+            
+            return tostring(readfile(self.Folder .. "Executions.txt"))
+        end)
+    end
+
+    function KleenexUtilities:GetExecutorHWID()
+        task.spawn(function()
+            local ExecutorHWID = get_hwid and get_hwid() or gethwid and gethwid() or hwid
+            self:CheckFunctionExists(ExecutorHWID)
+
+            return ExecutorHWID
+        end)
+    end
+
+    function KleenexUtilities:GetExecutor()
+        task.spawn(function()
+            local Executor = identifyexecutor and identifyexecutor() or getexecutorname and getexecutorname()
+            self:CheckFunctionExists(Executor)
+
+            return Executor
+        end)
+    end
+
+    function KleenexUtilities:GetCurrentGame()
+        return game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name
+    end
+
+    function KleenexUtilities:GetGameJobId()
+        return game.JobID
+    end
+end
+
+return KleenexUtilities
